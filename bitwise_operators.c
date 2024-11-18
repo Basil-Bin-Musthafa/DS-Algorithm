6b7883ece67a6889a89bd14feb5965fb9f8902da
Basil-Bin-Musthafa <malappurambasil@gmail.com>
2024-11-18 19:38:59 +0530

    program to reverse the elemets of an array

---
+++ bitwise_operators.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+
+void calculate_the_maximum(int n, int k) 
+{
+  int maxAND =0, maxOR =0, maxXOR=0, temp=0;
+  for(int i=1; i<n; i++)
+  {
+      for(int j=i+1; j<=n; j++)
+      {
+          temp = i&j;
+          maxAND = ((temp > maxAND) && (temp < k)) ? temp : maxAND;
+          
+          temp = i|j;
+          maxOR = ((temp > maxOR) && (temp < k)) ? temp : maxOR;
+          
+          temp = i^j;
+          maxXOR = ((temp > maxXOR) && (temp < k)) ? temp : maxXOR;
+      }
+  }
+  printf("%d\n%d\n%d",maxAND,maxOR,maxXOR);
+}
+
+int main() 
+{
+    int n, k;
+  
+    scanf("%d %d", &n, &k);
+    calculate_the_maximum(n, k);
+ 
+    return 0;
+}
